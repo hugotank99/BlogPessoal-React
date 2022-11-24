c075dfde474e3b857bd674f6eaf630d8b11b92d1
hugotank99 <hugoramosjan@gmail.com>
2022-11-24 11:36:01 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Title } from './componentes/title/Title';
+import { Text } from './componentes/Text/Text';
+import  Navbar from "./componentes/Navbar/Navbar";
+
 function App() {
+  const nome = 'hugo';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    <Navbar/>
+      <Title nome={nome}/>
+      <Text/>
+    </>
   );
 }
 
---
+++ src/componentes/Text/Text.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Text.css';
+
+function Text() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore animi architecto officiis harum tenetur voluptas quo voluptate. Consequatur, in ratione! Labore architecto saepe odit ex. Praesentium nemo porro sunt veritatis.
+      </p>
+    </>
+  )
+}
+
+export { Text };
